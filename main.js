72fa87feef4a46e608ad5fb679a3283fd9c07f97
Karthikeyan Balan <37651390+balankarthikeyan@users.noreply.github.com>
2026-02-15 01:07:02 +0530

    code updated

--- main.js
+++ main.js
@@ -1,59 +1,24 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const dotenv_1 = __importDefault(require("dotenv"));
-const db_1 = __importDefault(require("./db"));
-const SwaggerLayer_1 = __importDefault(require("./SwaggerLayer"));
-const getListAPI_1 = __importDefault(require("./getListAPI"));
-const getGetListAPI_1 = __importDefault(require("./getGetListAPI"));
-const PORT = (process.env.PORT || 9000);
-dotenv_1.default.config();
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-const allowedOrigins = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "https://your-frontend-domain.com",
-];
-// ✅ DB Instance
-const dataBase = new db_1.default();
-// ✅ Swagger instance
-const SwaggerLayerKit = new SwaggerLayer_1.default();
-/* ---------------- DB INIT (IMPORTANT) ---------------- */
-const onUpdateDBBase = () => {
-    dataBase.MONGODB_URL =
-        "mongodb+srv://admin:admin@simba-cluster.wv87zgs.mongodb.net";
-    dataBase.dbName = "Simba_Sample";
-    dataBase.collectionName = "simba_sample";
-    dataBase.doConnectInit();
-};
-/* ---------------- Swagger INIT ---------------- */
-const onUpdateSwagger = () => {
-    SwaggerLayerKit.app = app;
-    SwaggerLayerKit.doInit();
-};
-/* ---------------- CORS ---------------- */
-app.use((0, cors_1.default)({
-    origin: (origin, callback) => {
-        if (origin === null || origin === void 0 ? void 0 : origin.includes("localhost"))
-            return callback(null, true);
-        if (allowedOrigins.includes(origin) || !origin)
-            return callback(null, true);
-        callback(new Error("Not allowed by CORS"));
-    },
-}));
-/* ---------------- INIT BEFORE ROUTES ---------------- */
-onUpdateDBBase();
-onUpdateSwagger();
-/* ---------------- ROUTES ---------------- */
-(0, getListAPI_1.default)({ app, dataBase });
-(0, getGetListAPI_1.default)({ app, dataBase });
-// app.listen(PORT, () => {
-//   console.log(`I am listening on port ${PORT}`)
-// })
-/* ---------------- EXPORT FOR VERCEL ---------------- */
-exports.default = app;
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
+app.get("/:name", (req, res) => {
+    const { name } = req.params;
+    res.status(200).send(`Name: ${name}`);
+});
+app.get("/", (req, res) => {
+    res.send(`App is working fine`);
+});
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!");
+});
